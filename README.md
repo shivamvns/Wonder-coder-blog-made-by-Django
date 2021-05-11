76f74070a1e2d28e9e4d31b73d456dcb8a1151d0
shivam srivastava <shivamsrivastava033@gmail.com>
2021-05-11 13:58:27 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Wonder-coder-blog
+ 
---
+++ myblog/static/js/blog.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event){
+    console.log("hello everyone");
+
+
+    let sc = document.createElement('script')
+    sc.setAttribute('src', 'https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js');
+
+    document.head.appendChild(sc);
+    sc.onload = ()=>{
+        tinymce.init({
+            selector: '#id_content' 
+        });
+
+    }
+
+    
+
+
+});
+
+
